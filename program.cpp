e615705ab1f4d497f9caf5043f1c381eb3b713e5
vanshtyagi609 <vansh.tyagi30104@gmail.com>
2026-05-02 19:23:05 +0530

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,13 @@
+// wrt to print the numbers from 1 to 10 while using for loop
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
